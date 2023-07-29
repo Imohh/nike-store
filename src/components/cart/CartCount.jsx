952184a1705c9f77977bb89d05f055d0e1ff40b2
Imoh Precious <jaypee88830@gmail.com>
2023-07-29 02:38:33 +0100

    cart structure

---
+++ src/components/cart/CartCount.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartCount = () => {
+	return (
+		<>
+
+		</>
+	)
+}
+
+export default CartCount
---
+++ src/components/cart/CartEmpty.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartEmpty = () => {
+	return (
+		<>
+
+		</>
+	)
+}
+
+export default CartEmpty
